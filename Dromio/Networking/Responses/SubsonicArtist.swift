2405cf883b67ed132224bb717cb08af73f47a750
Matt Neuburg <matt@tidbits.com>
2025-03-10 09:10:04 -0700

    Revert "withdraw the artists interface"
    This reverts commit e38715c04871d36273f4340de30aab2c32306f65.

--- Dromio/Networking/Responses/SubsonicArtist.swift
+++ Dromio/Networking/Responses/SubsonicArtist.swift
@@ -4,7 +4,7 @@ struct SubsonicArtist: Codable, Equatable {
     let id: String
     let name: String
     let albumCount: Int?
-    // let album: [SubsonicAlbum]? // present only when getting a specific artist by id
+    let album: [SubsonicAlbum]? // present only when getting a specific artist by id
     let roles: [String]?
     var sortName: String?
 }
