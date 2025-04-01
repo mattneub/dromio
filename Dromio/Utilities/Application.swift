5bb4861dba2726067365277152c6063f3d3e9916
Matt Neuburg <matt@tidbits.com>
2025-04-01 07:06:18 -0700

    tweaking the attempt to make the app build for archiving

--- Dromio/Utilities/Application.swift
+++ Dromio/Utilities/Application.swift
@@ -3,7 +3,10 @@ import UIKit
 /// Protocol describing UIApplication for purposes of background tasks.
 @MainActor
 public protocol ApplicationType {
+    // weirdly, have to specify iOS here because tvOS variant is different; I have no idea why Xcode thinks we would be building for tvOS
+    @available(iOS 7.0, *)
     @MainActor func beginBackgroundTask(expirationHandler handler: (@MainActor @Sendable () -> Void)?) -> UIBackgroundTaskIdentifier
+    @available(iOS 7.0, *)
     @MainActor func beginBackgroundTask(withName taskName: String?, expirationHandler handler: (@MainActor @Sendable () -> Void)?) -> UIBackgroundTaskIdentifier
     @MainActor func endBackgroundTask(_ identifier: UIBackgroundTaskIdentifier)
 }
