2bfed9148677145669606c05e00c4a4d08414d94
Matt Neuburg <matt@tidbits.com>
2025-08-02 08:02:41 -0700

    still messing with that weird ApplicationType compile bug

--- Dromio/Protocols/Application.swift
+++ Dromio/Protocols/Application.swift
@@ -4,7 +4,7 @@ import UIKit
 @MainActor
 public protocol ApplicationType {
     // weirdly, have to specify iOS here because tvOS variant is different; I have no idea why Xcode thinks we would be building for tvOS
-    @available(iOS 7.0, *)
+    @available(iOS 18.0, *)
     @MainActor func beginBackgroundTask(expirationHandler handler: (@MainActor @Sendable () -> Void)?) -> UIBackgroundTaskIdentifier
     @MainActor func endBackgroundTask(_ identifier: UIBackgroundTaskIdentifier)
 }
